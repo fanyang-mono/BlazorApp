d9978714290a73c679e4f9f7a0b33549e2f2b2ff
Fan Yang <yangfan@microsoft.com>
2021-09-24 15:14:10 -0400

    Add CRUD blazor app

---
+++ BlazorCRUDApp2/BlazorCRUDApp2/Server/Program.cs
@@ -0,0 +1,44 @@
+using BlazorCRUDApp2.Shared;
+using BlazorCRUDApp2.Shared.Models;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Configuration.AddJsonFile("appsettings1.json",optional:false,reloadOnChange:true);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
---
+++ BlazorCRUDApp2/BlazorCRUDApp2/Shared/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BlazorCRUDApp2.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDApp2.Shared
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet <Car> Cars {  get; set; }
+    }
+}
