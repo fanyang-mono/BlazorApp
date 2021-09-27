beaeb6a1196307d0b5e4bafb9bb0c9a4d5943eb5
Fan Yang <yangfan@microsoft.com>
2021-09-27 15:23:44 -0400

    Add CRUD operations

---
+++ BlazorCRUDApp2/BlazorCRUDApp2/Server/Controllers/CarController.cs
@@ -0,0 +1,56 @@
+﻿using BlazorCRUDApp2.Shared;
+using BlazorCRUDApp2.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCRUDApp2.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarController:ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+
+        public CarController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Car>>> Get()
+        {
+            return await dbContext.Cars.ToListAsync();
+        }
+
+        [HttpGet("{id}",Name = "GetCar")]
+        public async Task<ActionResult<Car>> Get(int id)
+        {
+            return await dbContext.Cars.SingleOrDefaultAsync(x => x.id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Car car)
+        {
+            dbContext.Add(car);
+            await dbContext.SaveChangesAsync();
+            return new CreatedAtRouteResult("GetCar", new { id = car.id }, car);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Car car)
+        {
+            dbContext.Entry(car).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var car = new Car { id = id };
+            dbContext.Remove(car);
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ BlazorCRUDApp2/BlazorCRUDApp2/Server/Migrations/20210927151558_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorCRUDApp2.Shared.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
